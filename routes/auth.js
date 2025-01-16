64564b58a53568d7682468865b4434cd50c3e857
kelvinrono <ronohkelvin99@gmail.com>
2025-01-16 20:09:11 +0300

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login } from '../controllers/authControllers.js';
+import { register, login } from '../Controllers/authControllers.js';
 
 const router = express.Router();
 
