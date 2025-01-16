99501376d2c10c593c7430b741a882dc154c3b05
kelvinrono <ronohkelvin99@gmail.com>
2025-01-16 21:11:09 +0300

    fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: ['https://therapy-app-swart.vercel.app/', 'https://therapy-app-t4cp.onrender.com'],
   credentials: true
 }));
 app.use('/api/auth', authRoutes)
