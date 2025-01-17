6250717ccf1b75fa5f82d205532fa386bb11add0
kelvinrono <ronohkelvin99@gmail.com>
2025-01-17 12:05:08 +0300

    fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors({
-  origin: ['https://therapy-app-swart.vercel.app/', 'https://therapy-app-t4cp.onrender.com'],
+  origin: ['https://wazicare-fe.vercel.app/', 'https://therapy-app-t4cp.onrender.com'],
   credentials: true
 }));
 app.use('/api/auth', authRoutes)
